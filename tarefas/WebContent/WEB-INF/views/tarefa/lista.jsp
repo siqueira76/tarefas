84cfe2cd000856e99bada15e6e4999e22efbc09e
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2018-05-26 15:23:54 -0300

    Login

--- tarefas/WebContent/WEB-INF/views/tarefa/lista.jsp
+++ tarefas/WebContent/WEB-INF/views/tarefa/lista.jsp
@@ -6,6 +6,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script type="text/javascript" src="resources/js/jquery.js"></script>
 <title>Insert title here</title>
 </head>
 <body>
@@ -26,18 +27,17 @@
 			<th>Id</th>
 			<td>Descrição</td>
 			<th>Finalizado</th>
-			<th>Data de Finalização</th><tr>
-		
+			<th>Data de Finalização</th>
+		<tr>
+
 			<c:forEach items="${tarefas}" var="tarefa">
 				<tr>
 					<td>${tarefa.id}</td>
 					<td>${tarefa.descricao}</td>
 
 					<c:if test="${tarefa.finalizado eq false}">
-						
-						<td>
-							Não finalizado
-						</td>
+						<td id="tarefa_${tarefa.id}"><a href="#"
+							onclick="finalizaAgora(${tarefa.id})"> Finaliza Agora </a></td>
 					</c:if>
 
 					<c:if test="${tarefa.finalizado eq true}">
@@ -45,14 +45,12 @@
 					</c:if>
 
 					<td><fmt:formatDate value="${tarefa.dataFinalizacao.time}"
-							pattern="dd/MM/yyyy" />
-					</td>
-					<td><a href="removeTarefa?id=${tarefa.id}">Remover</a></td>
-					<td><a href="mostraTarefa?id=${tarefa.id}">Alterar</a></td>			
+							pattern="dd/MM/yyyy" /></td>
+					<td><a href="removeTarefa?id=${tarefa.id}" onclick="">Remover</a></td>
+					<td><a href="mostraTarefa?id=${tarefa.id}">Alterar</a></td>
 				</tr>
 
 			</c:forEach>
-	
 	</table>
 </body>
 </body>
--- tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
+++ tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -2,16 +2,26 @@ package br.com.caelum.tarefas.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.caelum.tarefas.dao.JdbcTarefaDao;
 import br.com.caelum.tarefas.modelo.Tarefa;
 
 @Controller
 public class TarefasController {
+	
+	private final JdbcTarefaDao dao;
+	
+	@Autowired
+	public TarefasController(JdbcTarefaDao dao ) {
+		this.dao = dao;
+	}
 
 	@RequestMapping("novaTarefa")
 	public String form() {
@@ -26,7 +36,7 @@ public class TarefasController {
 			return "tarefa/formulario";
 		}
 		
-		JdbcTarefaDao dao = new JdbcTarefaDao();
+		//JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
 		return "tarefa/adicionada";
 	}
@@ -34,7 +44,7 @@ public class TarefasController {
 	@RequestMapping("listaTarefas")
 	public String lista(Model model){
 		
-		JdbcTarefaDao dao = new JdbcTarefaDao();
+		//JdbcTarefaDao dao = new JdbcTarefaDao();
 		model.addAttribute("tarefas", dao.lista());
 		
 		return "tarefa/lista";
@@ -42,7 +52,7 @@ public class TarefasController {
 	
 	@RequestMapping("removeTarefa")
 	public String remove(Tarefa tarefa){
-		JdbcTarefaDao dao = new JdbcTarefaDao();
+		//JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.remove(tarefa);
 		return "redirect: listaTarefas";
 		
@@ -50,7 +60,7 @@ public class TarefasController {
 	
 	@RequestMapping("mostraTarefa")
 	public String mostra(Long id, Model model){
-		JdbcTarefaDao dao = new JdbcTarefaDao();
+		//JdbcTarefaDao dao = new JdbcTarefaDao();
 		model.addAttribute("tarefa", dao.buscaPorId(id));
 		return "tarefa/mostra";
 		
@@ -58,11 +68,19 @@ public class TarefasController {
 	
 	@RequestMapping("alteraTarefa")
 	public String altera(Tarefa tarefa){
-		JdbcTarefaDao dao = new JdbcTarefaDao();
+		//JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.altera(tarefa);
 		return "redirect:listaTarefas";
 		
 	}
+	
+	@ResponseBody
+	@RequestMapping("finalizaTarefa")
+	public void finaliza(Long id){
+		//JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.finaliza(id);
+	}
+	
 	
 	
 }
